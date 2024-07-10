3cd6c32c803b873ccfda21ad6dbe0ae8817c2a1d
Aview17 <846607819@qq.com>
2024-07-10 16:59:51 +0800

    test

--- mimikatz/mimikatz.c
+++ mimikatz/mimikatz.c
@@ -76,7 +76,7 @@ void mimikatz_begin()
 	kprintf(L"\n"
 		L"  .#####.   " MIMIKATZ_FULL L"\n"
 		L" .## ^ ##.  " MIMIKATZ_SECOND L" - (oe.eo)\n"
-		L" ## / \\ ##  /*** Benjamin DELPY `gentilkiwi` ( benjamin@gentilkiwi.com )\n"
+		L" ## \ \\ ##  /*** Benjamin DELPY `gentilkiwi` ( benjamin@gentilkiwi.com )\n"
 		L" ## \\ / ##       > https://blog.gentilkiwi.com/mimikatz\n"
 		L" '## v ##'       Vincent LE TOUX             ( vincent.letoux@gmail.com )\n"
 		L"  '#####'        > https://pingcastle.com / https://mysmartlogon.com ***/\n");
